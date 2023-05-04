ed5c064e467d1595aa58242f1a92021a935fa159
Krystian Zajac <tiocassi1@wp.pl>
2023-05-04 19:35:21 +0200

    added sample tasks

--- src/App.js
+++ src/App.js
@@ -10,6 +10,10 @@ const tasks = [];
 
 function App() {
   const [hideDone, setHideDone] = useState(false);
+  const [tasks, setTasks] = useState([
+    { id: 1, content: "przejść na Reacta", done: false },
+    { id: 2, content: "zjeść pierogi", done: true },
+  ]);
 
   const toggleHideDone = () => {
     setHideDone(hideDone => !hideDone);
