2ce3fd38487d526cabfe455571e57edeb83d3857
Krystian Zajac <tiocassi1@wp.pl>
2023-12-20 16:44:42 +0100

    adding empty task blocked

--- src/Form/index.js
+++ src/Form/index.js
@@ -6,12 +6,11 @@ const Form = ({ addNewTask }) => {
 
     const onFormSubmit = (event) => {
         event.preventDefault();
-
-        const trimmedNewTaskContent = newTaskContent.trim();
-
-        if (!trimmedNewTaskContent) {
+        setNewTaskContent("");
+        if (newTaskContent.trim() === "") {
             return;
-        };
+        }
+
 
         addNewTask(newTaskContent.trim());
         setNewTaskContent("");
